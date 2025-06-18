6daee405108a331e68c0d7c5f8c8a2615d265a93
mohrsalt <mohor001@e.ntu.edu.sg>
2025-06-18 23:24:57 +0400

    data linking fixed

--- VQ-GAN/taming/data/custom.py
+++ VQ-GAN/taming/data/custom.py
@@ -7,37 +7,38 @@ from monai.data import Dataset as MonaiDataset
 
 brats_transforms = transforms.Compose(
     [
-        transforms.LoadImaged(keys=["t1", "t1ce", "t2", "flair"], allow_missing_keys=True),
-        transforms.EnsureChannelFirstd(keys=["t1", "t1ce", "t2", "flair"], allow_missing_keys=True),
-        transforms.Lambdad(keys=["t1", "t1ce", "t2", "flair"], func=lambda x: x[0, :, :, :]),
-        transforms.AddChanneld(keys=["t1", "t1ce", "t2", "flair"]),
-        transforms.EnsureTyped(keys=["t1", "t1ce", "t2", "flair"]),
-        transforms.Orientationd(keys=["t1", "t1ce", "t2", "flair"], axcodes="RAI", allow_missing_keys=True),
-        transforms.CropForegroundd(keys=["t1", "t1ce", "t2", "flair"], source_key="t1", allow_missing_keys=True),
-        transforms.SpatialPadd(keys=["t1", "t1ce", "t2", "flair"], spatial_size=(160, 160, 128), allow_missing_keys=True),
-        transforms.RandSpatialCropd( keys=["t1", "t1ce", "t2", "flair"],
-            roi_size=(160, 160, 128),
-            random_center=True, 
-            random_size=False,
-        ),
-        transforms.ScaleIntensityRangePercentilesd(keys=["t1", "t1ce", "t2", "flair"], lower=0, upper=99.75, b_min=0, b_max=1),
+        transforms.LoadImaged(keys=["t1n", "t1c", "t2w", "t2f"], allow_missing_keys=True),
+        transforms.EnsureChannelFirstd(keys=["t1n", "t1c", "t2w", "t2f"], allow_missing_keys=True),
+        transforms.Lambdad(keys=["t1n", "t1c", "t2w", "t2f"], func=lambda x: x[0, :, :, :]),
+        transforms.AddChanneld(keys=["t1n", "t1c", "t2w", "t2f"]),
+        transforms.EnsureTyped(keys=["t1n", "t1c", "t2w", "t2f"]),
+        transforms.Orientationd(keys=["t1n", "t1c", "t2w", "t2f"], axcodes="RAI", allow_missing_keys=True),
+        transforms.CropForegroundd(keys=["t1n", "t1c", "t2w", "t2f"], source_key="t1", allow_missing_keys=True),
+        # transforms.SpatialPadd(keys=["t1n", "t1c", "t2w", "t2f"], spatial_size=(160, 160, 128), allow_missing_keys=True),
+        # transforms.RandSpatialCropd( keys=["t1n", "t1c", "t2w", "t2f"],
+        #     roi_size=(160, 160, 128),
+        #     random_center=True, 
+        #     random_size=False,
+        # ),
+        transforms.ScaleIntensityRangePercentilesd(keys=["t1n", "t1c", "t2w", "t2f"], lower=0, upper=99.75, b_min=0, b_max=1),
     ]
 )
 
-def get_brats_dataset(data_path):
+def get_brats_dataset(data_paths):
     transform = brats_transforms 
     
     data = []
-    for subject in os.listdir(data_path):
-        sub_path = os.path.join(data_path, subject)
-        if os.path.exists(sub_path) == False: continue
-        t1 = os.path.join(sub_path, f"{subject}_t1.nii.gz") 
-        t1ce = os.path.join(sub_path, f"{subject}_t1ce.nii.gz") 
-        t2 = os.path.join(sub_path, f"{subject}_t2.nii.gz") 
-        flair = os.path.join(sub_path, f"{subject}_flair.nii.gz") 
-        seg = os.path.join(sub_path, f"{subject}_seg.nii.gz")
+    for data_path in data_paths:
+        for subject in os.listdir(data_path):
+            sub_path = os.path.join(data_path, subject)
+            if os.path.exists(sub_path) == False: continue
+            t1n = os.path.join(sub_path, f"{subject}_t1n.nii.gz") 
+            t1c = os.path.join(sub_path, f"{subject}_t1c.nii.gz") 
+            t2w = os.path.join(sub_path, f"{subject}_t2w.nii.gz") 
+            t2f = os.path.join(sub_path, f"{subject}_t2f.nii.gz") 
+            seg = os.path.join(sub_path, f"{subject}_seg.nii.gz")
 
-        data.append({"t1":t1, "t1ce":t1ce, "t2":t2, "flair":flair, "subject_id": subject})
+            data.append({"t1n":t1n, "t1c":t1c, "t2w":t2w, "t2f":t2f, "subject_id": subject})
                     
     print("num of subject:", len(data))
 
--- plan.md
+++ plan.md
@@ -1,4 +1,6 @@
 Understand why no decoder in stage 2.....done
 log imges fn change...done
 check functional sim
-configure_optim fn change
+configure_optim fn change... done
+data linking and reading
+metrics
