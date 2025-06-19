38c39fefd1c8a897e4caa0d17cc37cb6ae3c47fb
mohrsalt <mohor001@e.ntu.edu.sg>
2025-06-19 10:16:04 +0400

    oom

--- VQ-GAN/taming/data/custom.py
+++ VQ-GAN/taming/data/custom.py
@@ -13,13 +13,13 @@ brats_transforms = transforms.Compose(
         transforms.AddChanneld(keys=["t1n", "t1c", "t2w", "t2f"]),
         transforms.EnsureTyped(keys=["t1n", "t1c", "t2w", "t2f"]),
         transforms.Orientationd(keys=["t1n", "t1c", "t2w", "t2f"], axcodes="RAI", allow_missing_keys=True),
-        transforms.CropForegroundd(keys=["t1n", "t1c", "t2w", "t2f"], source_key="t1", allow_missing_keys=True),
-        # transforms.SpatialPadd(keys=["t1n", "t1c", "t2w", "t2f"], spatial_size=(160, 160, 128), allow_missing_keys=True),
-        # transforms.RandSpatialCropd( keys=["t1n", "t1c", "t2w", "t2f"],
-        #     roi_size=(160, 160, 128),
-        #     random_center=True, 
-        #     random_size=False,
-        # ),
+        transforms.CropForegroundd(keys=["t1n", "t1c", "t2w", "t2f"], source_key="t1n", allow_missing_keys=True),
+        transforms.SpatialPadd(keys=["t1n", "t1c", "t2w", "t2f"], spatial_size=(160, 160, 128), allow_missing_keys=True),
+        transforms.RandSpatialCropd( keys=["t1n", "t1c", "t2w", "t2f"],
+             roi_size=(160, 160, 128),
+             random_center=True, 
+             random_size=False,
+         ),
         transforms.ScaleIntensityRangePercentilesd(keys=["t1n", "t1c", "t2w", "t2f"], lower=0, upper=99.75, b_min=0, b_max=1),
     ]
 )
@@ -29,14 +29,15 @@ def get_brats_dataset(data_paths):
     
     data = []
     for data_path in data_paths:
+        
         for subject in os.listdir(data_path):
             sub_path = os.path.join(data_path, subject)
             if os.path.exists(sub_path) == False: continue
-            t1n = os.path.join(sub_path, f"{subject}_t1n.nii.gz") 
-            t1c = os.path.join(sub_path, f"{subject}_t1c.nii.gz") 
-            t2w = os.path.join(sub_path, f"{subject}_t2w.nii.gz") 
-            t2f = os.path.join(sub_path, f"{subject}_t2f.nii.gz") 
-            seg = os.path.join(sub_path, f"{subject}_seg.nii.gz")
+            t1n = os.path.join(sub_path, f"{subject}-t1n.nii.gz") 
+            t1c = os.path.join(sub_path, f"{subject}-t1c.nii.gz") 
+            t2w = os.path.join(sub_path, f"{subject}-t2w.nii.gz") 
+            t2f = os.path.join(sub_path, f"{subject}-t2f.nii.gz") 
+            seg = os.path.join(sub_path, f"{subject}-seg.nii.gz")
 
             data.append({"t1n":t1n, "t1c":t1c, "t2w":t2w, "t2f":t2f, "subject_id": subject})
                     
